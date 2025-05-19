8f941447c9c063c7371ab18a5c0ead8e406db228
Jimmy Abraham <abrahamjimmy@hotmail.com>
2025-05-19 16:54:25 +0200

    Added more menu scenes: 	Main menu 	Options 	In-game menu (not working as no game yet)
    Added button functionality to switch between different menu's

---
+++ scripts/menus/titlescreen.gd
@@ -0,0 +1,18 @@
+extends MarginContainer
+
+var MAIN_MENU: String = "res://scenes/menus/main_menu.tscn"
+var OPTIONS: String = "res://scenes/menus/options.tscn"
+
+func _on_continue_button_pressed() -> void:
+	
+	pass # Replace with function body.
+
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_file(MAIN_MENU)
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file(OPTIONS)
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.
