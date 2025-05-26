69317f9348f9eed37b8f239a46cd5daed20e161c
Sebastian Nilsson <seblerr@gmail.com>
2025-05-26 14:39:46 +0200

    feat: add basic FSM for enemy actions

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,15 +1,63 @@
 extends CharacterBody2D
 
-@export var speed: int = 1
-@onready var player: Node2D = get_node("/root/World/Player")
+enum State {IDLE, CHASING, ATTACKING, STUNNED, DYING}
 
-func _physics_process(_delta: float) -> void:
-	if player:
-		var direction = (player.global_position - global_position).normalized()
-		velocity = direction * speed
-		#move_and_slide()
+const DEFAULT_SPEED := 60
+const CHASE_DISTANCE := 150
+const INITIAL_STATE := State.IDLE
 
-func _on_hurtbox_body_entered(body: Node2D) -> void:
-	# 'body' is the object that entered the Area2D (e.g. the Player)
-	if body.is_in_group("Player"):
-		print("aaaajjj")
+@export var speed: float = DEFAULT_SPEED
+
+var player: Node2D
+var state: State = INITIAL_STATE
+
+func _ready() -> void:
+	player = get_node("/root/World/Player")
+
+func _physics_process(delta: float) -> void:
+	match state:
+		State.IDLE:
+			state_idle(delta)
+		State.CHASING:
+			state_chase(delta)
+		State.ATTACKING:
+			# TODO: Implement attack
+			#state_attack(delta)
+			pass
+		State.STUNNED:
+			# TODO: Implement stunned
+			#state_stun(delta)
+			pass
+		State.DYING:
+			# TODO: Implement dying :(
+			#state_die(delta)
+			pass
+
+func state_idle(_delta: float) -> void:
+	if is_instance_valid(player):
+		print(global_position.distance_to(player.global_position))
+		if global_position.distance_to(player.global_position) < 150:
+			set_state(State.CHASING)
+
+func state_chase(_delta: float) -> void:
+	if not is_instance_valid(player):
+		return
+
+	var distance := global_position.distance_to(player.global_position)
+	if distance > CHASE_DISTANCE:
+		set_state(State.IDLE)
+
+	# TODO: Implement attacking based on distance to player
+
+	var direction := (player.global_position - global_position).normalized()
+	velocity = direction * speed
+	move_and_slide()
+
+func set_state(new_state: State) -> void:
+	if (new_state == state):
+		return
+	state = new_state
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.get_parent().is_in_group("Player"):
+		print("Ajj!")
